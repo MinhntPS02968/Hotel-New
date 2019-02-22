7d9ca6d03a7d43399cb3d0b2ff4ebf33ab78896a
minhntps02968 <minhntps02968@fpt.edu.vn>
2019-02-22 18:31:44 +0700

    update template

---
+++ header.php
@@ -0,0 +1,88 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+  <div class="container-fluid">
+  	<a class="navbar-brand col-md-2" href="#">
+  		<img class="img-fluid" src="img/logo.png">
+  	</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="col-md-10 collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">SPECIAL OFFERS</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          OUR PROPERTIES
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        	<ul class="dropdown-item">
+        		<div class="row pb-1 mb-1 border-bottom"><a href="#">Ha Noi</a></div>
+        		<li class="my-1"><a href="#">Hanoi La Siesta Trendy Hotel & Spa</a></li>
+        		<li class="my-1"><a href="#">Hanoi La Siesta Hotel & Spa</a></li>
+        		<li class="my-1"><a href="#">Hanoi La Siesta Trendy Hotel & Spa</a></li>
+        		<li class="my-1"><a href="#">Hanoi La Siesta Hotel & Spa</a></li>
+        		<li class="my-1"><a href="#">Hanoi La Siesta Trendy Hotel & Spa</a></li>
+        		<div class="row pb-1 mb-1 border-bottom"><a href="#">Hoi An</a></div>
+        		<li class="my-1"><a href="#">Hoi An La Siesta Trendy Hotel & Spa</a></li>
+        		<li class="my-1"><a href="#">Hoi An La Siesta Hotel & Spa</a></li>
+        	</ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT US</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          SERVICES
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        	<ul class="dropdown-item">
+        		<li class="my-1"><a href="#">Transfer Service</a></li>
+        		<li class="my-1"><a href="#">Vietnam Visa</a></li>
+        		<li class="my-1"><a href="#">The Den Bar</a></li>
+        		<li class="my-1"><a href="#">Diamond Sky Bar</a></li>
+        		<li class="my-1"><a href="#">La Spa</a></li>
+        		<li class="my-1"><a href="#">Cooking Class</a></li>
+        		<li class="my-1"><a href="#">Red Bean Restaurant</a></li>
+        	</ul>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          TOURS
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        	<ul class="dropdown-item">
+        		<li class="my-1"><a href="#">Transfer Service</a></li>
+        		<li class="my-1"><a href="#">Vietnam Visa</a></li>
+        		<li class="my-1"><a href="#">The Den Bar</a></li>
+        		<li class="my-1"><a href="#">Diamond Sky Bar</a></li>
+        		<li class="my-1"><a href="#">La Spa</a></li>
+        		<li class="my-1"><a href="#">Cooking Class</a></li>
+        		<li class="my-1"><a href="#">Red Bean Restaurant</a></li>
+        	</ul>
+        </div>
+      </li>
+    </ul>
+    <div class="navbar-nav">
+    	<div class="nav-item">
+        	<a class="nav-link" href="#">EHG NEWS</a>
+      </div>
+    </div>
+    <div class="navbar-nav">
+        	<span class="text-light">|</span>
+    </div>
+    <div class="navbar-nav">
+    	<div class="nav-item">
+        	<a class="nav-link" href="#">Contact Us</a>
+      </div>
+    </div>
+    <div class="navbar-nav">
+    	<div class="nav-item">
+    		<a href="#" class="btn btn-light" role="button">RESERVATION</a>
+      </div>
+    </div>
+  </div>
+</nav>
+	
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/popper.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<style>
+	/* Make the image fully responsive */
+	.carousel-inner img {
+		width: 100%;
+		height: 100%;
+	}
+</style>
+<title>Hotel</title>
+</head>
+<body>
+<?php 
+	include 'header.php';
+?>
+	<section>
+	</section>
+<?php 
+	include 'footer.php';
+?>
+</body>
+</html>
