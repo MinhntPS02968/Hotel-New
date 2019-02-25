4407c794677af4ab85a39a743733a29b7d3dbdb4
minhntps02968 <minhntps02968@fpt.edu.vn>
2019-02-25 17:33:21 +0700

    update code footer

--- footer.php
+++ footer.php
@@ -1,7 +1,31 @@
 <footer>
-    <div class="container pt-4">
+    <div class="container pt-5">
         <div class="row">
-            <div class="col-md-4">dsad</div>
+            <div class="col-md-4">
+            	<div class="row mb-3 pb-1 adress">
+            		<h6>EHG</h6>
+            		<ul class="pl-0">
+            			<li>HEAD OFFICE: Asia Tower, Unit 402, 6 Nha Tho Str., Hanoi, Vietnam</li>
+            			<li>Tel: (+84 24) 3862 2222</li>
+            			<li>Hotline: (+84) 936 453 684</li>
+            			<li>Email: <a href="#">dosm@elegancehospitalitygroup.com</a></li>
+            		</ul>
+            	</div>
+            	<div class="row">
+            		<div class="col-sm-12"><h6 class="row">FIND US ON</h6></div>
+            		<ul class="social col-sm-8 pl-0">
+            			<li class="facebook float-left mr-2">
+            				<a href="#">facebook</a>
+            			</li>
+            			<li class="instagram float-left mr-2">
+            				<a href="#">instagram</a>
+            			</li>
+            			<li class="youtube float-left mr-2">
+            				<a href="#">youtube</a>
+            			</li>
+            		</ul>
+            	</div>
+            </div>
             <div class="col-md-4">dsad</div>
             <div class="col-md-4">dsad</div>
         </div>
